ab4cc77db791a04681d2ab1a9c432837d40f9632
wasesasegara <wasesasegara@gmail.com>
2023-07-24 17:38:37 +0700

    wip, textfield validator

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -28,22 +28,40 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  return 'Demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters.';
+                  }
+                  return null;
                 },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
+                    flex: 1,
                     child: TextFormField(
+                      textAlign: TextAlign.center,
                       decoration: const InputDecoration(
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid positive number.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
                   Expanded(
+                    flex: 3,
                     child: DropdownButtonFormField(items: [
                       for (final category in categories.entries)
                         DropdownMenuItem(
@@ -62,6 +80,22 @@ class _NewItemState extends State<NewItem> {
                         ),
                     ], onChanged: (value) {}),
                   )
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Add Item'),
+                  ),
                 ],
               ),
             ],
